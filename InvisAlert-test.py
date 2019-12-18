3c0bce6b8b6aad13ee6161fa7a91e0c025eb50ea
Young CHi <young.c.chi@gmail.com>
2019-12-18 09:50:26 -0500

    completed - largest in array

--- InvisAlert-test.py
+++ InvisAlert-test.py
@@ -1,3 +1,15 @@
-print('Hello Invisalert')
-sum = 42 + 12
-print 'Sum of' 42 'and' 12 'is' sum
+# print string - Hello Invisalert!
+print('Hello Invisalert'!)
+
+# sum of num1 and num2
+num1 = 42
+num2 = 12
+sum = num1 + num2
+print ("Sum of",num1,"and",num2,"is",sum)
+
+# product of num1 and num2
+product = num1 * num2
+print ("Product of",num1,"and",num2,"is",product)
+
+# Largest number in array [10, 427, 12, 80, 572]
+array = [10, 427, 12, 80, 572]
