d048e9041264eba0a7b832dba7df5dec9a72a6f8
Young CHi <young.c.chi@gmail.com>
2019-12-18 10:03:44 -0500

    completed - palindrome

--- InvisAlert-test.py
+++ InvisAlert-test.py
@@ -1,5 +1,5 @@
 # print string - Hello Invisalert!
-print('Hello Invisalert'!)
+print('Hello, Invisalert!')
 
 # sum of num1 and num2
 num1 = 42
@@ -13,3 +13,30 @@ print ("Product of",num1,"and",num2,"is",product)
 
 # Largest number in array [10, 427, 12, 80, 572]
 array = [10, 427, 12, 80, 572]
+length = len(array)
+# python function to find maximum 
+def largestInArray(array, length): 
+  
+    # Initialize maximum element 
+    largest = array[0] 
+
+    # loop through array elements from second and compare with every element
+    for i in range(1, length): 
+        if array[i] > largest: 
+            largest = array[i] 
+    return largest
+  
+result = largestInArray(array,length) 
+print ("Largest in given array is",result)
+
+# palindrome
+# given a string, reverse the string and check to match the original string
+def palindrome(str):
+    reverseStr = str[::-1]
+    if (str == reverseStr):
+        return str + " is a palindrome"
+    return str + " is not a palindrome"
+
+string = "racecar"
+palindromeTest = palindrome(string)
+print (palindromeTest)
